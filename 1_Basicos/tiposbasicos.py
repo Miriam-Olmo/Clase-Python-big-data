fcd947eb940be017bf9ecd16857658a1d5bd5ae3
Miriam Olmo <miriam.olmofernandez@gmail.com>
2026-04-16 13:25:47 +0200

    creacion y modificacion de 1_basicos

---
+++ 1_Basicos/tiposbasicos.py
@@ -0,0 +1,28 @@
+nombre_alumno ="Miriam Olmo"
+print(nombre_alumno) # Miriam Olmo
+print(type(nombre_alumno))
+
+nombre_alumno =22
+print(nombre_alumno)
+print(type(nombre_alumno))
+
+
+# tipos basicos en python
+# numericos: almacenan numeros
+
+edad = 36
+grados = -15
+precio = 199.99
+
+
+#booleano: solo si/no, verdadero/falso
+
+estado = True
+
+
+# cadena de caracteres - string
+
+nombre = "Irene"
+apellidos = " Martinez"
+mensaje = 'El niño dijo : "Qué pasa"'
+print(mensaje)
