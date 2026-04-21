e6fd4a7a6e5b742c226f9ff11a8b17a57d339148
Miriam Olmo <miriam.olmofernandez@gmail.com>
2026-04-21 20:28:10 +0200

    añado ejercicios de practica en casa

--- 2_Flujo/bucles/ejercicio3for.py
+++ 2_Flujo/bucles/ejercicio3for.py
@@ -1,42 +1,19 @@
-"""El Asno, el Perro y el Lobo
+# quiero pedir un texto por pantalla de cualquier longitud y quiero que pidais un vocal
+# contar cuantas vocales tiene el texto.
 
-Caminaban penosamente bajo el Sol, un asno con su carga y el amo seguido del perro.
-
-Llegados a la pradera, el amo cansado se echó a dormir; el burro, entonces, se puso a pacer libremente y solo el perro quedó en peor estado que cuando andaba.
-
-—Compañero, amigo —le dijo al asno— ¿por qué no me haces un espacio para tomar algo de la cesta? El burro respondió, —¿por qué no te esperas un poquito a que despierte el amo y él te sirva la merienda?
-
-De repente, varió la situación por completo, pues un lobo, que acechaba al grupo, se arrojó sobre el cuello del asno.
-
-—¡Socórreme, compañero —gritaba el burro en su agonía—, pero el perro, contemplando la escena desde una altura, repuso: —¿por qué no te esperas un poquito a que despierte el amo y te socorra?
-"""
-
-
-# 1 pedir texto por pantalla
-
-texto = input('dame tu frase: ').lower()
-print(texto)
-
-# 2 pedir una vocal
-
-vocal = input('dame una vocal: ').lower()
-print(vocal)
-
-# 3 pintar todos los caracteres del texto
+# paso 1: Pedir el texto por pantalla
+texto = input('Dame una frase maravillosa: ').lower()
+# paso 2: Pedir una vocal
+vocal = input('Dame una vocal: ').lower()
 
 cantidad = len(texto)
-for i in range(cantidad):
-    print(texto[i]) 
-
-# 4 imprimir solo vocal pedida
-for i in range(cantidad):
-    if texto[i] == vocal:
-    print(texto[i])
-
-# 5 contar las vocales. crear una variable que incremente en 1 cada vez que encuentre mi vocal
-
+# paso 3: pintar todos los caracteres del texto
+# paso 4: imprimir solo la vocal pedida
+# paso 5: contar las vocales. crear un variable que incremente en 1 cada vez que encuentre mi vocal
 numero_vocales = 0
-
+# paula  - a
 for i in range(cantidad):
     if texto[i] == vocal:
-        numero_vocales = numero_vocales + 1
+        numero_vocales = numero_vocales + 1 
+        # numero_vocales += 1
+print(numero_vocales)
---
+++ ejercicios_casa/for_caracteres.py
@@ -0,0 +1,10 @@
+# 1. Definimos la palabra
+
+palabra = "practica"
+
+# 2. Usamos el bucle for para recorrer cada carácter (letra)
+
+print(len(palabra) )
+
+for i in range(len(palabra)):
+    print(palabra[i])
