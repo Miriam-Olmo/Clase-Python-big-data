d185d0e22d4eeffc725a1304b1d3d2b83f928c47
Miriam Olmo <miriam.olmofernandez@gmail.com>
2026-04-26 20:35:40 +0200

    practica en casa

--- 2_Flujo/funciones/ejercicio4_funciones.py
+++ 2_Flujo/funciones/ejercicio4_funciones.py
@@ -1,4 +1,10 @@
+# Realizar una funcion que introduzca el nombre de una persona y me devuelve solo sus iniciales
+# ej: Juan Antonio Perez -> "J.A.P."
 
+# paso 1: la primera letra del nombre simpre es la primera inicial
+# paso 2: recorrer la cadena de caracteres
+# paso 3: buscar lo que separa cada nombre y apellido el espacio
+# paso 4: la letra justo despues del espacio es la inicial a convertir
 
 
 
