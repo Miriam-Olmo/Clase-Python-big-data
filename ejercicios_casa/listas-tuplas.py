ac54d0a60af56054a1c87cf79eb1fadd2afd9876
Miriam Olmo <miriam.olmofernandez@gmail.com>
2026-05-03 12:24:01 +0200

    practica en casa

---
+++ ejercicios_casa/listas-tuplas.py
@@ -0,0 +1,14 @@
+# Ejercicio 1
+# Escribir un programa que almacene las asignaturas de un curso (por ejemplo Matemáticas, Física, Química, Historia y Lengua) en una lista y la muestre por pantalla.
+
+
+materias = ['Matematicas', 'lenguaje', 'historia', 'quimica', 'fisica', 'arte']
+
+print(materias)
+
+
+# Ejercicio 2
+# Escribir un programa que almacene las asignaturas de un curso (por ejemplo Matemáticas, Física, Química, Historia y Lengua) en una lista y la muestre por pantalla el mensaje Yo estudio <asignatura>, donde <asignatura> es cada una de las asignaturas de la lista.
+
+for materia in materias:
+    print('yo estudio ' + materia)
