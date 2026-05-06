9eac3aa4e8e5718f1e8a4d27e035b6018319a84c
Miriam Olmo <miriam.olmofernandez@gmail.com>
2026-05-06 22:00:41 +0200

    calculada nominas

--- ejercicios_clase/ejercicio_4/ej4.py
+++ ejercicios_clase/ejercicio_4/ej4.py
@@ -6,7 +6,7 @@ def calcular_coste_hora_extra(trabajador):
     # multiplicamos por las horas extra y obtenemos el coste_horas_extra
     # se lo añadimos al trabajo clave: valor
     coste_horas_extra = trabajador['sueldo_base'] / trabajador['horas_contrato']
-    total_horas_extra = coste_horas_extra * {trabajador['horas_extra']}
+    total_horas_extra = coste_horas_extra * trabajador['horas_extra']
     trabajador['total_horas_extra'] = total_horas_extra
     print(trabajador)
 
@@ -15,6 +15,15 @@ for trabajador in trabajadores:
 
 #calcular nomina de un trabajador
 
-# calcular nomina de todos los trabajadores
+def calcular_nomina(trabajador):
+    bruto = trabajador['sueldo_base'] + trabajador['total_horas_extra']
+    IRPF = trabajador['sueldo_base'] * (trabajador['porcentaje_impuestos'] / 100)
+    neto_sin_extras = bruto - IRPF
+    resultado_neto = neto_sin_extras + trabajador['total_horas_extra']
+    trabajador['resultado_neto'] = resultado_neto
+    print(trabajador)
 
+# calcular nomina de todos los trabajadores
+for trabajador in trabajadores:
+    calcular_nomina(trabajador)
 # horas extra sin impuestos
