a4dba1e3ffa823134ca10fcd1b4b12f27aee2dfb
Miriam Olmo <miriam.olmofernandez@gmail.com>
2026-05-17 19:09:25 +0200

    practica en casa

--- ejercicios_casa/listas-tuplas.py
+++ ejercicios_casa/listas-tuplas.py
@@ -18,3 +18,17 @@ for materia in materias:
 # Ejercicio 3
 # Escribir un programa que almacene las asignaturas de un curso (por ejemplo Matemáticas, Física, Química, Historia y Lengua) en una lista, pregunte al usuario la nota que ha sacado en cada asignatura, y después las muestre por pantalla con el mensaje En <asignatura> has sacado <nota> donde <asignatura> es cada una des las asignaturas de la lista y <nota> cada una de las correspondientes notas introducidas por el usuario.
 
+asignaturas = ['física', 'química', 'lenguaje', 'arte', 'biología']
+notas = []
+
+# Pedimos las notas
+for asignatura in asignaturas:
+    nota = input(f"¿Qué nota has sacado en {asignatura}? ")
+    notas.append(nota)
+
+# Mostramos los resultados iterando por ambas listas
+for i in range(len(asignaturas)):
+    print(f"En {asignaturas[i]} has sacado {notas[i]}")
+
+
+
