b9f09c98ff97181eeb46cbbf295eec72a0de3fd2
Miriam Olmo <miriam.olmofernandez@gmail.com>
2026-05-18 19:13:10 +0200

    funciones ejercicio 10 hechas (sin corregir)

--- ejercicios_clase/ejercicio_10/lib/functions.py
+++ ejercicios_clase/ejercicio_10/lib/functions.py
@@ -12,4 +12,19 @@ def limpiar_texto(valor, mayusculas = False):
         return 'sin datos'
     valor = str(valor).strip()
     return valor.upper( ) if mayusculas else valor.lower()
+# 'precio': limpiar_precio(item['precio']),
+# 'stock': limpiar_stock(item['stock'])
+def  limpiar_precio(precio):
+    precio = float(precio)
+    if precio == 'gratis':
+        return 0.0
+    elif precio != 'gratis':
+        return precio
+    else:
+        print('no hay precio registrado')
+    return precio
 
+def limpiar_stock(stock):
+    stock = int(stock)
+    if stock == '-' or 'N/A':
+        return 0
